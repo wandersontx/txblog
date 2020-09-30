a6b5607e08725c9002ae849dd3877e53d1594f83
wandersontx <wandersonh89@gmail.com>
2020-09-30 00:01:00 -0300

    Projeto criado

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel com bootstrap</title>
+
+    <link rel="stylesheet" href="{{ asset('site/style.css') }}">
+</head>
+<body>
+
+    <h1 class="display-2 text-orange2">Testando boostrap</h1>
+ <button class="btn btn-lg btn-warning">ok</button>
+ 
+<script src="{{ asset('site/jquery.js') }}"></script>
+<script src="{{ asset('site/bootstrap.js') }}"></script>
+</body>
+</html>
